3221a4b879cc88096d20fc11c56186b5c00fdd31
ChrisTomaTheFirst <cristian.toma1@emag.ro>
2020-08-07 13:32:33 +0300

    added readmy

--- app/import.sh
+++ app/import.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Note that false sets $? to 1
+# set $? to 1
 false
 while [ $? -ne 0 ]; do
     php /var/www/html/app/products_update.php
-#    php /var/www/html/app/test.php >> log.log
 done
